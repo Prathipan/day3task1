b9e6ffc076dd8e8577b42d7a49e45153bdcaa38a
Prathipan <83485567+Prathipan@users.noreply.github.com>
2021-12-26 15:01:09 +0530

    Add files via upload

---
+++ JSONresume.js
@@ -0,0 +1,55 @@
+const resume_data ={
+    "basics": {
+      "name": "Prathipan P",
+      "label": "Programmer",
+      "email": "prathipan24p@gmail.com",
+      "phone": "7904460317",
+      "location": {
+        "address": "MAdurai",
+        "postalCode": "625012",
+        "city": "Madurai",
+      },
+      "Hobby": [{
+        "name": "Drawing",
+      }]
+    },
+    "work": [{
+      "name": "HCL Technologies",
+      "position": "Analyst",
+      "startDate": "18-07-2021"
+    }],
+    "education": [{
+      "institution": "KLN College of Engineering",
+      "url": "https://www.klnce.edu/",
+      "area": "Computer science and engineering",
+      "studyType": "Bachelor",
+      "startDate": "2016",
+      "endDate": "2020",
+      "CGPA": "7.32",
+    }],
+    "skills": [{
+      "name": "Programing",
+      "Interest": "Web development",
+      "keywords": [
+        "C/C++",
+        "HTML",
+        "CSS",
+        "JavaScript"
+      ]
+    }],
+    "languages": [{
+      "language": ["English","Tamil"]
+    }],
+    "projects": [{
+      "name": "Women safety system",
+      "description": "Smart watch which helps when women is in danger",
+      "keywords": [
+        "Arduino"
+      ],
+      "startDate": "Jan-2020",
+      "endDate": "Mar-2020",
+      "roles": [
+        "Team Lead"
+      ]
+    }]
+  }
